534fee978adb99808abece33f7d73402318223f1
jilin_jjc <51039078+jinjinchi@users.noreply.github.com>
2025-11-05 11:05:34 +0800

    Add files via upload

---
+++ src/edim_train.py
@@ -0,0 +1,181 @@
+import argparse
+import numpy as np
+import os
+import random
+import torch
+import torch.nn as nn
+import glob
+from ruamel.yaml import YAML
+from src.losses.EDIM_loss import EDIMLoss
+from src.models.EDIM import EDIM
+from src.utils.Card_dataloader import CardDataset
+from src.trainer.EDIM_trainer import EDIMTrainer, freeze_grad_and_eval
+from src.neural_networks.encoder import BaseEncoderSH
+import h5py
+from sklearn.model_selection import train_test_split
+
+
+def run(
+    xp_name: str,
+    conf_path: str,
+    data_base_folder: str,
+    trained_enc_x_path: str,
+    trained_enc_y_path: str,
+    seed: int = None,
+):
+    yaml = YAML(typ='safe', pure=True)  
+    with open(conf_path, "r") as f:
+        conf = yaml.load(f)  
+    if seed is not None:
+        seed = seed
+        random.seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.cuda.manual_seed_all(seed)
+        print("seed",seed)
+
+    TRAINING_PARAM = conf["training_param"]
+    MODEL_PARAM = conf["model_param"]
+    LOSS_PARAM = conf["loss_param"]
+    SHARED_PARAM = conf["shared_param"]
+    batch_size = TRAINING_PARAM["batch_size"]
+
+    kernel_size = 2
+    img_size = (MODEL_PARAM["img_size"] - 3 + 2) // 1 + 1
+    img_size1 = (img_size - kernel_size) // 2 + 1
+    img_size2 = (img_size1 - kernel_size) // 2 + 1
+    img_size3 = (img_size2 - kernel_size) // 2 + 1
+
+    trained_enc_x = BaseEncoderSH(
+        img_size1=img_size1,
+        img_size2=img_size2,
+        img_size3=img_size3,
+        in_channels=3,
+        num_filters=16,
+        kernel_size=kernel_size,
+        repr_dim=SHARED_PARAM["shared_dim"],
+    )
+    trained_enc_y = BaseEncoderSH(
+        img_size1=img_size1,
+        img_size2=img_size2,
+        img_size3=img_size3,
+        in_channels=3,
+        num_filters=16,
+        kernel_size=kernel_size,
+        repr_dim=SHARED_PARAM["shared_dim"],
+    )
+    
+    trained_enc_x.load_state_dict(torch.load(trained_enc_x_path))
+    trained_enc_y.load_state_dict(torch.load(trained_enc_y_path))
+    freeze_grad_and_eval(trained_enc_x)
+    freeze_grad_and_eval(trained_enc_y)
+    print("share encoder x:", trained_enc_x_path)
+    print("share encoder y:", trained_enc_y_path)
+
+    edim = EDIM(
+        img_size=MODEL_PARAM["img_size"],
+        channels=MODEL_PARAM["channels"],
+        shared_dim=SHARED_PARAM["shared_dim"],
+        exclusive_dim=MODEL_PARAM["exclusive_dim"],
+        trained_encoder_x=trained_enc_x,
+        trained_encoder_y=trained_enc_y,
+        batch_size=TRAINING_PARAM["batch_size"],
+    )
+    loss = EDIMLoss(
+        local_mutual_loss_coeff=LOSS_PARAM["local_mutual_loss_coeff"],
+        global_mutual_loss_coeff=LOSS_PARAM["global_mutual_loss_coeff"],
+        disentangling_loss_coeff=LOSS_PARAM["disentangling_loss_coeff"],
+    )
+
+    # Load data
+    dataset_path_train = os.path.join(data_base_folder, 'Card_pair_dataset_train_no_outlier.h5')
+    dataset_path_test = os.path.join(data_base_folder, 'Card_pair_dataset_test.h5')
+    if not os.path.isfile(dataset_path_train):
+        raise FileNotFoundError(
+            f"{dataset_path} not found. Please place the dataset in this directory.")
+
+    print("LoveDA loading")
+    with h5py.File(dataset_path_train, 'r') as dataset:
+        images_pair_train = dataset['images'][:]
+        factors_pair_train = dataset['labels'][:]
+        
+    with h5py.File(dataset_path_test, 'r') as dataset:
+        images_pair_test = dataset['images'][:]
+        factors_pair_test = dataset['labels'][:]
+    print("LoveDA finish")
+    
+    print("dataset size:",images_pair_train.shape)
+    print(factors_pair_train.shape)
+        
+    train_dataset = CardDataset(images_pair_train, factors_pair_train)
+    test_dataset = CardDataset(images_pair_test, factors_pair_test)
+
+    device = TRAINING_PARAM["device"]
+    learning_rate = TRAINING_PARAM["learning_rate"]
+    batch_size = TRAINING_PARAM["batch_size"]
+    epochs = TRAINING_PARAM["epochs"]
+    trainer = EDIMTrainer(
+        dataset_train=train_dataset,
+        dataset_test=test_dataset,
+        model=edim,
+        loss=loss,
+        learning_rate=learning_rate,
+        batch_size=batch_size,
+        device=device,
+    )
+    trainer.train(epochs=epochs, xp_name=xp_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Learning Disentangled Representations via Mutual Information Estimation"
+    )
+    parser.add_argument(
+        "--xp_name",
+        nargs="?",
+        type=str,
+        default="Shared_training",
+        help="Mlflow experiment name",
+    )
+    parser.add_argument(
+        "--conf_path", nargs="?", type=str, default=None, help="Configuration file"
+    )
+    parser.add_argument(
+        "--data_base_folder", nargs="?", type=str, default=None, help="Data folder"
+    )
+
+    parser.add_argument("--seed", nargs="?", type=int, default=142, help="Random seed")
+    parser.add_argument(
+        "--trained_enc_x_path",
+        nargs="?",
+        type=str,
+        default=None,
+        help="Pretrained shared encoder x",
+    )
+    parser.add_argument(
+        "--trained_enc_y_path",
+        nargs="?",
+        type=str,
+        default=None,
+        help="Pretrained shared encoder y",
+    )
+
+    args = parser.parse_args()
+    xp_name = args.xp_name
+    conf_path = args.conf_path
+    data_base_folder = args.data_base_folder
+    seed = args.seed
+    trained_enc_x_path = args.trained_enc_x_path
+    trained_enc_y_path = args.trained_enc_y_path
+    run(
+        xp_name=xp_name,
+        conf_path=conf_path,
+        data_base_folder=data_base_folder,
+        trained_enc_x_path=args.trained_enc_x_path,
+        trained_enc_y_path=args.trained_enc_y_path,
+        seed=seed,
+    )
---
+++ src/sdim_train.py
@@ -0,0 +1,125 @@
+import argparse
+import numpy as np
+import os
+import random
+import torch
+import torch.nn as nn
+import glob
+from ruamel.yaml import YAML
+from src.losses.SDIM_loss import SDIMLoss
+from src.models.SDIM import SDIM
+from src.trainer.SDIM_trainer import SDIMTrainer
+from src.utils.Card_dataloader import CardDataset
+from sklearn.model_selection import train_test_split
+import h5py
+from collections import Counter
+
+
+def run(
+        xp_name: str,
+        conf_path: str,
+        data_base_folder: str,
+        seed: int = None,
+):
+    yaml = YAML(typ='safe', pure=True)  
+    with open(conf_path, "r") as f:
+        conf = yaml.load(f)  
+    if seed is not None:
+        seed = seed
+        random.seed(seed)
+        os.environ["PYTHONHASHSEED"] = str(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+        torch.cuda.manual_seed_all(seed)
+        print(seed)
+
+    TRAINING_PARAM = conf["training_param"]
+    MODEL_PARAM = conf["model_param"]
+    LOSS_PARAM = conf["loss_param"]
+    batch_size = TRAINING_PARAM["batch_size"]
+
+    sdim = SDIM(
+        img_size=MODEL_PARAM["img_size"],
+        channels=MODEL_PARAM["channels"],
+        shared_dim=MODEL_PARAM["shared_dim"],
+        switched=MODEL_PARAM["switched"],
+        batch_size=batch_size
+    )
+    loss = SDIMLoss(
+        local_mutual_loss_coeff=LOSS_PARAM["local_mutual_loss_coeff"],
+        global_mutual_loss_coeff=LOSS_PARAM["global_mutual_loss_coeff"],
+        shared_loss_coeff=LOSS_PARAM["shared_loss_coeff"],
+    )
+
+    # Load data
+    dataset_path_train = os.path.join(data_base_folder, 'Card_pair_dataset_train_no_outlier.h5')
+    dataset_path_test = os.path.join(data_base_folder, 'Card_pair_dataset_test.h5')
+    if not os.path.isfile(dataset_path_train):
+        raise FileNotFoundError(
+            f"{dataset_path} not found. Please place the dataset in this directory.")
+
+    print("LoveDA loading")
+    with h5py.File(dataset_path_train, 'r') as dataset:
+        images_pair_train = dataset['images'][:]
+        factors_pair_train = dataset['labels'][:]
+        
+    with h5py.File(dataset_path_test, 'r') as dataset:
+        images_pair_test = dataset['images'][:]
+        factors_pair_test = dataset['labels'][:]
+    print("LoveDA finish")
+    
+    print("dataset size:",images_pair_train.shape)
+    print(factors_pair_train.shape)
+
+    train_dataset = CardDataset(images_pair_train, factors_pair_train)
+    test_dataset = CardDataset(images_pair_test, factors_pair_test)
+
+    device = TRAINING_PARAM["device"]
+    learning_rate = TRAINING_PARAM["learning_rate"]
+    epochs = TRAINING_PARAM["epochs"]
+    trainer = SDIMTrainer(
+        dataset_train=train_dataset,
+        dataset_test=test_dataset,
+        model=sdim,
+        loss=loss,
+        learning_rate=learning_rate,
+        batch_size=batch_size,
+        device=device,
+    )
+    trainer.train(epochs=epochs, xp_name=xp_name)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Learning Disentangled Representation via Robust Optimal Transport"
+    )
+    parser.add_argument(
+        "--xp_name",
+        nargs="?",
+        type=str,
+        default="Shared_training",
+        help="Mlflow experiment name",
+    )
+    parser.add_argument(
+        "--conf_path", nargs="?", type=str, default=None, help="Configuration file"
+    )
+    parser.add_argument(
+        "--data_base_folder", nargs="?", type=str, default=None, help="Data folder"
+    )
+    parser.add_argument("--seed", nargs="?", type=int, default=144, help="Random seed")
+
+    args = parser.parse_args()
+    xp_name = args.xp_name
+    conf_path = args.conf_path
+    data_base_folder = args.data_base_folder
+    seed = args.seed
+
+    run(
+        xp_name=xp_name,
+        conf_path=conf_path,
+        data_base_folder=data_base_folder,
+        seed=seed,
+    )

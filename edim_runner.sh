a69de681186e072933dff70794758efb51a1995b
jilin_jjc <51039078+jinjinchi@users.noreply.github.com>
2025-11-05 15:26:26 +0800

    Update edim_runner.sh

--- edim_runner.sh
+++ edim_runner.sh
@@ -2,7 +2,7 @@ echo Start exclusive representation training
 data_base_folder="data"
 xp_name="Exclusive_representation_training"
 conf_path="conf/exclusive_conf.yaml"
-trained_enc_x_path="/home/chijinjin/WDR--final/Test/05_Card_RobustWPC/share-ot-best/artifacts/sh_encoder_x/state_dict.pth"
-trained_enc_y_path="/home/chijinjin/WDR--final/Test/05_Card_RobustWPC/share-ot-best/artifacts/sh_encoder_y/state_dict.pth"
+trained_enc_x_path="mlruns/3/bc1ql88psd24kvqnykj29g9mc674a9prffjtlfdfem/artifacts/sh_encoder_x/state_dict.pth"
+trained_enc_y_path="mlruns/3/bc1ql88psd24kvqnykj29g9mc674a9prffjtlfdfem/artifacts/sh_encoder_y/state_dict.pth"
 
 PYTHONPATH=$PYTHONPATH:src python src/edim_train.py --xp_name $xp_name --conf_path $conf_path --data_base_folder $data_base_folder --trained_enc_x_path $trained_enc_x_path --trained_enc_y_path $trained_enc_y_path
